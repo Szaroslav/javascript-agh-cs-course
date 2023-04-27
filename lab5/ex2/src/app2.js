27e5ac40256c3b640ff2c6eada8634d8583aaf61
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-04-27 17:01:04 +0200

    Create `exercise 2` [`lab5`]

---
+++ lab5/ex2/src/app2.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const students = [
+    {
+          fname: 'Jan',
+          lname: 'Kowalski'
+    },
+    {
+          fname: 'Anna',
+          lname: 'Nowak'
+    },
+];
+const locals = { students: students };
+
+
+/* *************************** */
+/* Configuring the application */
+/* *************************** */
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set('views', __dirname + '/views'); // Files with views can be found in the 'views' directory
+app.set('view engine', 'pug'); // Use the 'Pug' template system
+app.locals.pretty = app.get('env') === 'development'; // The resulting HTML code will be indented in the development environment
+/* ************************************************ */
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public')); 
+
+/* ******** */
+/* "Routes" */
+/* ******** */
+app.get('/', function (request, response) {
+    response.render('index', locals); // Render the 'index' view
+});
+/* ************************************************ */
+app.listen(8000, function () {
+    console.log('The server was started on port 8000');
+    console.log('To stop the server, press "CTRL + C"');
+});
