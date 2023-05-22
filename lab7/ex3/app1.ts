b41bcbca18d80237cfb88762b23f6c0cfe8d33c5
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-05-22 23:07:29 +0200

    Tried to finish `ex3` [`lab7`]

---
+++ lab7/ex3/app1.ts
@@ -0,0 +1,56 @@
+// Requiring modules 
+import { Application, Router, Status, send } from "https://deno.land/x/oak@v12.5.0/mod.ts";       
+import { dejsEngine, oakAdapter, viewEngine } from "https://deno.land/x/view_engine@v10.6.0/mod.ts";
+// Initiate app
+const app = new Application();
+const router = new Router();
+
+// app.use(async (ctx, next) => {
+//     await send(ctx, ctx.request.url.pathname, {
+//         root: `${Deno.cwd()}/static`
+//     })
+//     next();
+// });
+
+app.use(viewEngine(oakAdapter, dejsEngine, { viewRoot: './views' }));
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.get("/", async (ctx) => {
+    await ctx.render('index.ejs' ,{ data: { msg: 'World' } });
+});
+
+router.all('/submit', async function (ctx) {
+    if (!ctx.request.hasBody) {
+        ctx.throw(Status.BadRequest, "Bad Request");
+    }
+
+    const { name } = ctx.request.body;
+    
+    // Return the greeting in the format preferred by the WWW client
+    switch (ctx.request.headers.get('Accept').match(/\/.*$/).slice(1)) {
+        case 'json':
+            // Send the JSON greeting
+            ctx.response.headers.set('Content-Type', 'application/json');
+            ctx.response.body = { welcome: `Hello '${name}'` };
+            console.log(`\x1B[32mThe server sent a JSON document to the browser using the '${ctx.request.method}' method\x1B[0m`);
+            break;
+
+        case 'xml':
+            // Send the XML greeting
+            ctx.response.headers.set('Content-Type', 'application/xml');
+            ctx.response.body = `<welcome>Hello '${name}'</welcome>`;
+            console.log(`\x1B[32mThe server sent an XML document to the browser using the '${ctx.request.method}' method\x1B[0m`);
+            break;
+
+        default:
+            // Send the text plain greeting
+            ctx.response.headers.set('Content-Type', 'text/plain');
+            ctx.response.body = `Hello '${name}'`;
+            console.log(`\x1B[32mThe server sent a plain text to the browser using the '${ctx.request.method}' method\x1B[0m`);
+    }
+});
+/* ************************************************ */
+
+console.log('App is listening to port: 8000');
+await app.listen({ port:8000 });
